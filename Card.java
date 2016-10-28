91b7ebf201376a9fa970e06e8c44d9773bb98ae4
CaribbeanMax <et.ef@gmx.net>
2016-10-28 15:44:36 +0200

    Add files via upload

--- Card.java
+++ Card.java
@@ -5,13 +5,16 @@ import handChecker.PokerCard;
 public class Card implements PokerCard{
 	Color color;
 	Value value;
+	
 	Card(Color c, Value v){
 		this.color = c;
 		this.value = v;
 	}
+	
 	public Color getColor(){
 		return this.color;
 	}
+	
 	public Value getValue(){
 		return this.value;
 	}
--- Deck.java
+++ Deck.java
@@ -1,14 +1,48 @@
 package main;
 
-public class Deck {
-	Card[] card = new Card[52];
-	Deck(){
-		int counter=0;
-		for (int i=0; i<13; i++){
-			for (int j=0; j<4; j++){
-				card[counter] = new Card(Card.Color.values()[j], Card.Value.values()[i]);
-				counter++;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Deck{
+	private LinkedList<Card> deck = new LinkedList<Card>();
+	
+	Deck(int cardCount){
+		switch (cardCount) {
+		case 52:
+			for (int i=0; i<13; i++){
+				for (int j=0; j<4; j++){
+					deck.add(new Card(Card.Color.values()[j], Card.Value.values()[i]));
+				}
 			}
+			break;
+		default:
+			break;
 		}
+	}
+	
+	public void shuffle(){
+		LinkedList<Card> tmp = new LinkedList<Card>();
+		Random rnd = new Random();
+		while(this.deck.isEmpty()==false){
+			tmp.add(this.deck.remove(rnd.nextInt(this.deck.size())));
+		}
+		this.deck = tmp;
+	}
+	
+	public Card draw(){
+		return this.deck.remove();
+	}
+	
+	public void add(Card c){
+		this.deck.addFirst(c);
+	}
+	
+	public List<Card> getList(){
+		return this.deck;
+	}
+	
+	public boolean isEmpty(){
+		return this.deck.isEmpty();
 	}
 }
