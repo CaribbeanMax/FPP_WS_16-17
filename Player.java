66871540dece2ffdeec1658aa9e4846607ba7bfd
SB <Sebastian.Bose@web.de>
2016-10-28 15:48:56 +0200

    Add files via upload
    not functioning

---
+++ Player.java
@@ -0,0 +1,10 @@
+package main;
+// 
+public class Player {
+	public int money;
+	public String playerName;
+Player(String name, Table.MONEYATBEGIN){ //Konstante in Table.java definieren
+	this.name=name; this.money=Table.MONEYATBEGIN;
+}
+
+}
