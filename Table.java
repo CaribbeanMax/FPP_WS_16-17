b0e85e279e637d7b21df2ad832d48e097873e9e3
SB <Sebastian.Bose@web.de>
2016-11-11 15:36:52 +0100

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,25 +1,74 @@
 package main;
 
 import java.util.LinkedList;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Table {
-	public final int MONEYATBEGIN=5000;
-	private boolean gameInProgress = false;
-	private int dealerOnSeat;
-Table(){
-	List<String> tableList = new List<String>();
-}
-void addPlayer(String Player.playerName){
-	if( gameInProgress == false ){
-		tableList.add(Player.playerName);
-}}
-void deletePlayer(String Player.playerName){
-	tableList.remove(Strig Player.playername);
-}
-void startGame(){
-	gameInProgress = true;
-	dealerOnSeat;//random
-}
-
-
+	private int dealer = -1;
+	private Card[] communityCards = new Card[5];
+	private LinkedList<Player> playerList = new LinkedList<Player>();
+	private int maxPlayers;
+	private Deck deck = new Deck(52);
+	private Deck discardPile = new Deck(0);
+	Table(int mP){
+		this.maxPlayers = mP;
+		
+	}
+	public void firstDealer(){
+		if(dealer == -1){
+			this.dealer = new Random().nextInt(playerList.size());
+		}
+	}
+	public boolean addPlayer(String name, int money, int seat){	
+		int counter = 0;
+		do{
+			if(counter < playerList.size()){
+				if(playerList.get(counter).getSeat() > seat){
+					playerList.add(counter, new Player(name, money, seat));
+					return false;
+				}else if(playerList.get(counter).getSeat() == seat){
+					return true;
+				}else{
+					counter++;
+				}
+			}else if(counter > maxPlayers){
+				return true;
+			}else{
+				playerList.addLast(new Player(name, money, seat));
+				return false;
+			}
+		}while(true);
+	}
+	public void deletePlayer(Player player){
+		playerList.remove(player);
+	}
+	public void pushDealer(){
+		if(dealer+1 < playerList.size()){
+			dealer ++;
+		}else{
+			dealer = 0; // dealer on first seat
+		}
+	}
+	public Player getDealer(){
+		return playerList.get(dealer);
+	}
+	public Player getPlayer(int index){
+		return playerList.get(index);
+	}
+	
+	public void showFlop(){
+		discardPile.add(deck.draw());// discardpile.add(); burncard
+		communityCards[0] = deck.draw();
+		communityCards[1] = deck.draw();
+		communityCards[2] = deck.draw();
+	}
+	public void showTurn(){
+		discardPile.add(deck.draw());// discardpile.add(); burncard
+		communityCards[3] = deck.draw();
+	}
+	public void showRiver(){
+		discardPile.add(deck.draw());// discardpile.add(); burncard
+		communityCards[4] = deck.draw();
+	}
 }
