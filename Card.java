2cfe49640417abaf85a55f04aee5a85544a4360e
CaribbeanMax <et.ef@gmx.net>
2016-11-18 14:25:58 +0100

    Update Card.java

--- Card.java
+++ Card.java
@@ -3,19 +3,20 @@ package main;
 import handChecker.PokerCard;
 
 public class Card implements PokerCard{
-	Color color;
-	Value value;
-	
+	private Color color;
+	private Value value;
 	Card(Color c, Value v){
 		this.color = c;
 		this.value = v;
 	}
-	
 	public Color getColor(){
 		return this.color;
 	}
-	
 	public Value getValue(){
 		return this.value;
+	}
+	@Override
+	public String toString(){
+		return "[" + color + " " + value + "]";
 	}
 }
