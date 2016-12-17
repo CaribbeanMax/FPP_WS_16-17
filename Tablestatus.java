265a0d2c6d349cfc4d99307381d16391d40b770d
CaribbeanMax <et.ef@gmx.net>
2016-12-17 12:34:13 +0100

    Create Tablestatus.java

---
+++ Tablestatus.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Tablestatus {
+	public String dealer;
+	public String current;
+	public Card[] communityCards;
+	public String[] playerNames;
+	public int[] playerMoney;
+	public int[] playerBet;
+	public boolean[] playerFolded;
+	public Card[] playerCards;
+	public int pot;
+	public boolean deckIsEmpty;
+	public boolean discardpileIsEmpty;
+	public int roundCounter;
+	public int roundsToBlindRaise;
+	public int smallBlind;
+}
