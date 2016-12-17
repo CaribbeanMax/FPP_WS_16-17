466fc2930fcf53932ba8c5ecec76bc764d84ddf5
CaribbeanMax <et.ef@gmx.net>
2016-12-17 12:33:35 +0100

    Create Communication.java

---
+++ Communication.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Communication {
+	public String varName;
+	public Object data;
+	public Communication(String vN, Object d){
+		this.varName = vN;
+		this.data = d;
+	}
+}
