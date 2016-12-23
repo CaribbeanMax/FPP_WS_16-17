0b99206438bab5df065f9bec2f2cb5fa4983fb0a
CaribbeanMax <et.ef@gmx.net>
2016-12-23 12:55:43 +0100

    Update Server.java

--- Server.java
+++ Server.java
@@ -24,18 +24,31 @@ public class Server extends Thread{
 		int roundsToBlindRaise = Integer.parseInt(JOptionPane.showInputDialog("Runden bis zur Blinderhöhung:", maxPlayers*2));
 		Table table = new Table(this,maxPlayers,smallBlind,roundsToBlindRaise);
 		try{
+			System.out.println("Server läuft und wartet auf Clients.");
 			listener = new ServerSocket(GAMEPORT);
 			do{
 				Socket newPlayer = listener.accept();
+				System.out.println("Neuer Client entdeckt!");
 				clientList.add(new Clientmanager(newPlayer,false, this));
 				clientList.getLast().start();
-			}while(maxPlayers<clientList.size());
+			}while(maxPlayers>clientList.size());
+			int readyPlayers = 0;
+			do{
+				readyPlayers = 0;
+				for (int i=0; i<clientList.size(); i++){
+					if (clientList.get(i).getSeat() != -1){
+						readyPlayers++;
+					}
+				}
+			}while(maxPlayers>readyPlayers);
 			table.start();
+			System.out.println("Spiel gestartet.");
 			do{
 				Socket newViewer = listener.accept();
 				clientList.add(new Clientmanager(newViewer,true, this));
 				clientList.getLast().start();
 			}while(table.isAlive());
+			System.out.println("Spiel beendet.");
 			do{
 				clientList.getLast().endConnection();
 				if(!clientList.getLast().isAlive()){
@@ -43,13 +56,29 @@ public class Server extends Thread{
 				}
 			}while(clientList.size()>0);
 			listener.close();
-		}catch(IOException e){}
+			System.out.println("Server wird beendet.");
+		}catch(IOException e){
+			System.out.println(e);
+		}
 	}
-	
-	public void sendUpdate(String varName, Object data){
+
+	public void sendUpdate(String varName, Serializable data){
 		for (int i=0; i<clientList.size(); i++){
 			clientList.get(i).sendUpdate(varName, data);
 		}
+	}
+	
+	public void sendUpdate(String varName, Serializable data, int seat){
+		int clientnr = -1;
+		int i = 0;
+		while (i<clientList.size() && clientnr == -1){
+			if (clientList.get(i).getSeat() == seat){
+				clientnr = i;
+			}else{
+				i++;
+			}
+		}
+		clientList.get(i).sendUpdate(varName, data);
 	}
 	
 	public Object sendQuestion(String varName, int seat){
@@ -58,13 +87,15 @@ public class Server extends Thread{
 		while (i<clientList.size() && clientnr == -1){
 			if (clientList.get(i).getSeat() == seat){
 				clientnr = i;
+			}else{
+				i++;
 			}
 		}
 		return clientList.get(i).sendQuestion(varName);
 	}
 
 	public String getPlayerName(int i){
-		return clientList.get(i).getName();
+		return clientList.get(i).getPName();
 	}
 	
 	public int getSeat(int i){
@@ -79,9 +110,10 @@ public class Server extends Thread{
 		}
 		return true;
 	}
+	
 	public boolean approve(int seat){
 		for(int i=0; i < clientList.size(); i++){
-			if (seat==(clientList.get(i).getSeat())){
+			if (seat == clientList.get(i).getSeat()){
 				return false;
 			}
 		}
