7e9a95c891bbc83edb8d2c685a054cb8e3790121
CaribbeanMax <et.ef@gmx.net>
2016-12-23 12:55:19 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -10,9 +10,9 @@ public class Client {
 		client.run();
 	}
 	
-	private Tablestatus tablestatus;
+	private Tablestatus tablestatus = new Tablestatus();
 	
-	public void	run(){
+	private void run(){
 		try {
 			Socket server = new Socket("localhost", Server.GAMEPORT);
 			ObjectInputStream in = new ObjectInputStream(server.getInputStream());
@@ -23,125 +23,141 @@ public class Client {
 				try{
 					lastHeard = in.readObject();
 				}catch (ClassNotFoundException e){
-					lastHeard = null;
+					System.out.println("Nachrichtenfehler!");
 				}
 				//Fallunterscheidung für Inputs
 				if (lastHeard instanceof Communication){
 					String varName = ((Communication)lastHeard).varName;
+					Serializable varData = ((Communication)lastHeard).data;
+					System.out.println(varName + " + " + varData);
 					switch(varName){
 						case "end":
 							end = true;
+							break;
+						case "winner":
+							
+							break;
 						case "tablestatus":
-							this.tablestatus = (Tablestatus)((Communication)lastHeard).data;
+							tablestatus = (Tablestatus)varData;
 							break;
 						case "dealer":
-							this.tablestatus.dealer = (String)((Communication)lastHeard).data;
+							tablestatus.dealer = (String)varData;
 							break;
 						case "current":
-							this.tablestatus.current = (String)((Communication)lastHeard).data;
+							tablestatus.current = (String)varData;
 							break;
 						case "communityCards":
-							InfoWithIndex tmpCC = ((InfoWithIndex)(((Communication)lastHeard).data));
-							this.tablestatus.communityCards[tmpCC.index] = (Card)tmpCC.data;
+							InfoWithIndex tmpCC = (InfoWithIndex)varData;
+							tablestatus.communityCards[tmpCC.index] = (Card)tmpCC.data;
 							break;
 						case "playerNames":
-							InfoWithIndex tmpPN = ((InfoWithIndex)(((Communication)lastHeard).data));
-							this.tablestatus.playerNames[tmpPN.index] = (String)tmpPN.data;
+							InfoWithIndex tmpPN = (InfoWithIndex)varData;
+							tablestatus.playerNames[tmpPN.index] = (String)tmpPN.data;
 							break;
 						case "playerMoney":
-							InfoWithIndex tmpPM = ((InfoWithIndex)(((Communication)lastHeard).data));
-							this.tablestatus.playerMoney[tmpPM.index] = (int)tmpPM.data;
+							InfoWithIndex tmpPM = (InfoWithIndex)varData;
+							tablestatus.playerMoney[tmpPM.index] = (int)tmpPM.data;
 							break;
 						case "playerBet":
-							InfoWithIndex tmpPB = ((InfoWithIndex)(((Communication)lastHeard).data));
-							this.tablestatus.playerBet[tmpPB.index] = (int)tmpPB.data;
+							InfoWithIndex tmpPB = (InfoWithIndex)varData;
+							tablestatus.playerBet[tmpPB.index] = (int)tmpPB.data;
 							break;
 						case "playerFolded":
-							InfoWithIndex tmpPF = ((InfoWithIndex)(((Communication)lastHeard).data));
-							this.tablestatus.playerFolded[tmpPF.index] = (boolean)tmpPF.data;
+							InfoWithIndex tmpPF = (InfoWithIndex)varData;
+							tablestatus.playerFolded[tmpPF.index] = (boolean)tmpPF.data;
 						case "playerCards":
-							this.tablestatus.playerCards = (Card[])((Communication)lastHeard).data;
+							InfoWithIndex tmpPC = (InfoWithIndex)varData;
+							tablestatus.playerCards[tmpPC.index] = (Card)tmpPC.data;
 							break;
 						case "pot":
-							this.tablestatus.pot = (int)((Communication)lastHeard).data;
+							tablestatus.pot = (int)varData;
 							break;
 						case "deckIsEmpty":
-							this.tablestatus.deckIsEmpty = (boolean)((Communication)lastHeard).data;
+							tablestatus.deckIsEmpty = (boolean)varData;
 							break;
 						case "discardpileIsEmpty":
-							this.tablestatus.discardpileIsEmpty = (boolean)((Communication)lastHeard).data;
+							tablestatus.discardpileIsEmpty = (boolean)varData;
 							break;
 						case "roundCounter":
-							this.tablestatus.roundCounter = (int)((Communication)lastHeard).data;
+							tablestatus.roundCounter = (int)varData;
 							break;
 						case "roundsToBlindRaise":
-							this.tablestatus.roundsToBlindRaise = (int)((Communication)lastHeard).data;
+							tablestatus.roundsToBlindRaise = (int)varData;
 							break;
 						case "smallBlind":
-							this.tablestatus.smallBlind = (int)((Communication)lastHeard).data;
+							tablestatus.smallBlind = (int)varData;
 							break;
 						case "question":
 							//Antworten
 							String[] option = {null,null,"Fold"};
-							switch((String)((Communication)lastHeard).data){
+							switch((String)varData){
 								case "name":
 									out.writeObject(JOptionPane.showInputDialog("Spielername:"));
 									break;
 								case "seat":
-									out.writeObject(JOptionPane.showInputDialog("Sitzplatznr:"));
+									out.writeObject(Integer.parseInt(JOptionPane.showInputDialog("Sitzplatznr:")));
 									break;
-								case "bet":
-									out.writeObject(JOptionPane.showInputDialog("Wieviel setzen?"));
+								case "inputBet":
+									out.writeObject(Integer.parseInt(JOptionPane.showInputDialog("Wieviel setzen?")));
 									break;
-								case "raise":
-									out.writeObject(JOptionPane.showInputDialog("Um wieviel erhöhen?"));
+								case "inputRaise":
+									out.writeObject(Integer.parseInt(JOptionPane.showInputDialog("Um wieviel erhöhen?")));
 									break;
-								case "allIn":
-									out.writeObject(JOptionPane.showInputDialog("All-In gehen?"));
+								case "inputAllIn":
+									out.writeObject(Integer.parseInt(JOptionPane.showInputDialog("All-In gehen?")));
 									break;
 								case "checkOrBet":
 									option[0] = "Check"; option[1] = "Bet";
-									out.writeObject(JOptionPane.showOptionDialog(null, ("Was möchten Sie tun?"), "",
-											JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null));
+									Integer tmpCB = JOptionPane.showOptionDialog(null, ("Was möchten Sie tun?"), "",
+											JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null);
+									out.writeObject(tmpCB);
 									break;
 								case "checkOrRaise":
 									option[0] = "Check"; option[1] = "Raise";
-									out.writeObject(JOptionPane.showOptionDialog(null, ("Was möchten Sie tun?"), "",
-											JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null));
+									Integer tmpCR = JOptionPane.showOptionDialog(null, ("Was möchten Sie tun?"), "",
+											JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null);
+									out.writeObject(tmpCR);
 									break;
 								case "callOrRaise":
 									option[0] = "Call"; option[1] = "Raise";
-									out.writeObject(JOptionPane.showOptionDialog(null, ("Was möchten Sie tun?"), "",
-											JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null));
+									Integer tmpCR2 = JOptionPane.showOptionDialog(null, ("Was möchten Sie tun?"), "",
+											JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null);
+									out.writeObject(tmpCR2);
 									break;
-								default: System.out.println("Didn't understand Question from server.");
+								default:
+									System.out.println("Didn't understand Question from server.");
+									break;
 							}
 							break;
-						default: System.out.println("Didn't understand Message from server.");
+						default:
+							System.out.println("Didn't understand Message from server: " + varName);
+							break;
 					}
 					if (!varName.equals("question")){
 						//print status falls neue Info
 						System.out.println("Small Blind: " + tablestatus.smallBlind + " Dealer: " + tablestatus.dealer
 							+ " Pot: " + tablestatus.pot);
-						for (int i = 0; i<tablestatus.playerNames.length; i++){
+						for (int i = 1; i<tablestatus.playerNames.length; i++){
 							System.out.println(tablestatus.playerNames[i] + " Money: " + tablestatus.playerMoney[i]
 								+ " Bet: " + tablestatus.playerBet[i] + " Folded: " + tablestatus.playerFolded[i]);
 						}
-						System.out.println("Tischkarten:");
+						System.out.print("Tischkarten: ");
 						for (int i = 0; i<5; i++){
 							System.out.print(" " + tablestatus.communityCards[i]);
 						}
 						System.out.println();
-						System.out.println("Ihre Karten: " + tablestatus.playerCards[0] + tablestatus.playerCards[1]);
+						System.out.println("Ihre Karten: " + tablestatus.playerCards[0] + " " + tablestatus.playerCards[1]);
 						System.out.println(tablestatus.current + " ist dran!");
 					}
+				} else {
+					System.out.println("Kommunikationsfehler");
 				}
 			}while(!end);
 			server.close();
 		} catch (UnknownHostException e) {
 			System.out.println("Can´t find host.");
 		} catch (IOException e) {
-			System.out.println("Error connecting to host."); 
+			System.out.println("Error connecting to host.\n" + e); 
 		}
 		//Siegeranzeige
 	}
