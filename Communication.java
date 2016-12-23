0ed6d2dc4cfec918fc8b92b9664c1dc9a63c2295
CaribbeanMax <et.ef@gmx.net>
2016-12-23 12:56:45 +0100

    Update Communication.java

--- Communication.java
+++ Communication.java
@@ -1,10 +1,17 @@
 package main;
 
-public class Communication {
-	public String varName;
-	public Object data;
-	public Communication(String vN, Object d){
+import java.io.*;
+
+public class Communication implements Serializable{
+	private static final long serialVersionUID = 1L;
+	public final String varName;
+	public final Serializable data;
+	public Communication(String vN, Serializable d){
 		this.varName = vN;
 		this.data = d;
+	}
+	@Override
+	public String toString(){
+		return ("Var: " + varName + " data: " + data);
 	}
 }
