bedb0a0b33701d812404fb4858dfeba00df0076f
CaribbeanMax <et.ef@gmx.net>
2016-12-23 12:54:50 +0100

    Update Clientmanager.java

--- Clientmanager.java
+++ Clientmanager.java
@@ -6,67 +6,86 @@ import java.io.*;
 public class Clientmanager extends Thread{
 	private Socket client;
 	private Server server;
-	private int seat;
+	private int seat = -1;
 	private String name;
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 	private boolean endConnection = false;
-	private boolean asked = false;
+	private boolean flip = false;
 	@SuppressWarnings("unused")	private boolean isViewer;
-	@SuppressWarnings("unused")	private Object lastHeard = null;
+	private Object lastHeard = null;
 	public Clientmanager(Socket c, boolean iV, Server s){
-		try {
-			client = c;
-			server = s;
-			in = new ObjectInputStream(client.getInputStream());
-			out = new ObjectOutputStream(client.getOutputStream());
-			String requestedName="";
-			do{
-				requestedName = (String)sendQuestion("name");
-			}while(!server.approve(requestedName));
-			name=requestedName;
-			int requestedSeat=-1;
-			do{
-				requestedSeat = (Integer)sendQuestion("seat");
-			}while(!server.approve(requestedSeat));
-			seat=requestedSeat;
-			
-		} catch (IOException e) {}
+		client = c;
+		server = s;
 		this.isViewer = iV;
 	}
 	
 	public void run(){
 		try {
+			out = new ObjectOutputStream(client.getOutputStream());
+			in = new ObjectInputStream(client.getInputStream());
 			//Begrüßung
-			
+			String requestedName;
 			do{
-				if (!asked){
-					lastHeard = in.readObject();
-				}
+				requestedName = (String)sendMyQuestion("name");
+			}while(!server.approve(requestedName));
+			name=requestedName;
+			int requestedSeat;
+			do{
+				requestedSeat = (int)sendMyQuestion("seat");
+			}while(!server.approve(requestedSeat));
+			seat=requestedSeat;
+			//Hören
+			do{
+				lastHeard = in.readObject();
+				System.out.println(lastHeard);
+				flip = !flip;
 			}while(!endConnection);
 			//Verabschiedung
 			
 			client.close();
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 	
-	public void sendUpdate(String varName, Object data){
+	public void sendUpdate(String varName, Serializable data){
 		Communication com = new Communication(varName, data);
+		System.out.println("U: " + com);
 		try{
 			out.writeObject(com);
-		}catch(IOException e){}
+		}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+	
+	private Object sendMyQuestion(String varName){
+		Communication com = new Communication("question",varName);
+		Object data = null;
+		System.out.println("F: " + com);
+		try{
+			out.writeObject(com);
+			data = in.readObject();
+		}catch(IOException e){
+			System.out.println("Error connecting to client.\n" + e); 
+		}catch(ClassNotFoundException e) {
+			System.out.println("Communication error.\n" + e);
+		}
+		return data;
 	}
 	
 	public Object sendQuestion(String varName){
 		Communication com = new Communication("question",varName);
 		Object data = null;
+		System.out.println("F: " + com);
 		try{
-			asked = true;
+			boolean tmp = flip;
 			out.writeObject(com);
-			data = in.readObject();
-			asked = false;
+			while (tmp == flip){}
+			data = lastHeard;
 		}catch(IOException e){
-		}catch(ClassNotFoundException e){}
+			System.out.println("Error connecting to client.\n" + e); 
+		}
 		return data;
 	}
 
