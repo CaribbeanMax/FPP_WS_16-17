0714620410597f0d658711c6429c90100888e999
CaribbeanMax <et.ef@gmx.net>
2016-12-23 12:57:40 +0100

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,10 +1,13 @@
 package main;
 
 import handChecker.PokerCard;
+import java.io.Serializable;
 
-public class Card implements PokerCard{
+public class Card implements PokerCard, Serializable{
+	private static final long serialVersionUID = 1L;
 	private Color color;
 	private Value value;
+	
 	Card(Color c, Value v){
 		this.color = c;
 		this.value = v;
